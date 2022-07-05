57a1a1b4290fea4314e339a7a9f24bdef13098ef
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-07-05 21:34:23 +0200

    New simplified design of game and grid

--- app.js
+++ app.js
@@ -1,79 +1,28 @@
 let gridWidth = 4;
-let squareGridWidth = 3;
-let parcelNumber = 5;
+let parcelNumber = 3;
+let invalidAmount = 3;
 let gridContainer = document.getElementById("grid-container");
-let emptySquares = [0,2,7,15];
-let validSquares = [1,3,4,5,6,8,9,10,11,12,13,14];
-let buildingSquares = [1,3,4,5,6,8,9,10,11,12,14];
-let eastRoad = [4,8,9,10,12,13];
-let westRoad = [5,9,10,11,13,14];
-let centralEWRoad = [13];
-let northRoad = [5,8,12,14];
-let southRoad = [1,4,8,10];
-let southEastRoad = [1,6];
-let northWestRoad = [6,11];
-let southWestRoad = [3];
-let northEastRoad = [6];
-let randomSquares = []
 
-for (let i=0; i<parcelNumber; i++) {
+
+/*for (let i=0; i<parcelNumber; i++) {
     let randomSquare = Math.floor(Math.random() * buildingSquares.length);
     randomSquares.push(buildingSquares[randomSquare])
-}
-//create grid
-for (let i = 0; i < gridWidth * gridWidth; i++) {
-    //create individual squares (including empty ones)
-    const square = document.createElement("div");
-    square.setAttribute("id", i);
-    square.classList.add("square");
-    gridContainer.appendChild(square);
-    if (emptySquares.includes(i)) {
-        square.classList.add("square-empty")
-    }
-    // create roads and buildings
-    if (validSquares.includes(i)) {
-        for (let j = 0; j < 9; j++) {
-            const roadBlock = document.createElement("div");
-            roadBlock.classList.add("road-block");
-            roadBlock.classList.add(j)
-            square.appendChild(roadBlock);
-            if (buildingSquares.includes(i) && j===4) {
-                roadBlock.classList.add("building")
-            }
-            if (eastRoad.includes(i) && j===5) {
-                roadBlock.classList.add("e-road")
-            }
-            if (westRoad.includes(i) && j===3) {
-                roadBlock.classList.add("w-road")
-            }
-            if (centralEWRoad.includes(i) && j===4) {
-                roadBlock.classList.add("w-road")
-            }
-            if (northRoad.includes(i) && j===1) {
-                roadBlock.classList.add("n-road")
-            }
-            if (southRoad.includes(i) && j===7) {
-                roadBlock.classList.add("s-road")
-            }
-            if (southEastRoad.includes(i) && j===8) {
-                roadBlock.classList.add("se-road")
-            }
-            if (northWestRoad.includes(i) && j===0) {
-                roadBlock.classList.add("nw-road")
-            }
-            if (southWestRoad.includes(i) && j===6) {
-                roadBlock.classList.add("sw-road")
-            }
-            if (northEastRoad.includes(i) && j===2) {
-                roadBlock.classList.add("ne-road")
-            }
-        }
-    }
-    // create parcles using randomSquares array
-    if (randomSquares.includes(i)) {
-        square.classList.add("parcel")
+}*/
+
+//create board
+function createBoard() {
+    const invalidArray = Array(invalidAmount).fill("empty");
+    const validArray = Array(gridWidth * gridWidth - invalidAmount).fill("square");
+    const gameArray = validArray.concat(invalidArray);
+    const shuffledArray = gameArray.sort(() => Math.random() -.5);
+    for (let i = 0; i < gridWidth * gridWidth; i++) {
+        //create individual squares (including empty ones)
+        const square = document.createElement("div");
+        square.setAttribute("id", i);
+        square.classList.add(shuffledArray[i]);
+        console.log(square)
+        gridContainer.appendChild(square);
     }
 }
 
-
-
+createBoard()
