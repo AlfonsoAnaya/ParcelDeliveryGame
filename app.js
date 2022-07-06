91c406bab81f8840728879efdd670f438309844a
AlfonsoAnaya <68916711+AlfonsoAnaya@users.noreply.github.com>
2022-07-06 12:56:06 +0200

    The creteGrid function now creates pareces with directions and current place

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let gridWidth = 4;
 let parcelNumber = 3;
 let invalidAmount = 3;
 let gridContainer = document.getElementById("grid-container");
+let squares = [];
+let validSquares = []
 
 
 /*for (let i=0; i<parcelNumber; i++) {
@@ -11,18 +13,43 @@ let gridContainer = document.getElementById("grid-container");
 
 //create board
 function createBoard() {
-    const invalidArray = Array(invalidAmount).fill("empty");
-    const validArray = Array(gridWidth * gridWidth - invalidAmount).fill("square");
+    const invalidArray = Array(invalidAmount).fill("square-empty");
+    const validArray = Array(gridWidth * gridWidth - invalidAmount).fill("square-valid");
     const gameArray = validArray.concat(invalidArray);
     const shuffledArray = gameArray.sort(() => Math.random() -.5);
+    //create individual squares (including empty ones)
     for (let i = 0; i < gridWidth * gridWidth; i++) {
-        //create individual squares (including empty ones)
         const square = document.createElement("div");
         square.setAttribute("id", i);
         square.classList.add(shuffledArray[i]);
-        console.log(square)
         gridContainer.appendChild(square);
+        squares.push(square);
     }
+    //create starting point 
+    validSquares = squares.filter(s => s.classList.contains("square-valid"));
+    validSquares[Math.floor(Math.random() * validSquares.length)].classList.add("current");
+    //create parcels
+    for (let i = 0; i < parcelNumber; i++) {
+        createParcel()
+    }
+    //addEventListeners to move
+    validSquares.forEach(s => s.addEventListener("click", ()=>move(s)))
+}
+
+function createParcel() {
+    let validParcelSquares = squares.filter(s => s.classList.contains("square-valid")).filter(s=> !s.classList.contains("current"));
+    const randomNumParcel = Math.floor(Math.random() * validParcelSquares.length);
+    validParcelSquares[randomNumParcel].classList.add("parcel");
+    const randomNumDestination = Math.floor(Math.random() * validParcelSquares.length);
+    validParcelSquares[randomNumDestination].classList.add("destination");
+}
+
+function move(newSquare) {
+    currentSquare = validSquares.find(s=>s.classList.contains("current"));
+    console.log(currentSquare);
+    currentSquare.classList.remove("current");
+    newSquare.classList.add("current");
+
 }
 
 createBoard()
